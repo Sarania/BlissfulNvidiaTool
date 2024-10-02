bba1d7d4b9eaf2642f12b37504e018cc7517756f
Blyss Sarania <blyss.sarania@gmail.com>
2024-10-02 11:15:24 -0400

    Make my OCD happy

--- blissnvidiatool.py
+++ blissnvidiatool.py
@@ -99,8 +99,8 @@ def draw_dashboard(stdscr):
         stdscr.addstr(7,0, "Utilization: ")
         stdscr.addstr(8,0, "VRAM Usage: ")
         # Display metrics
-        stdscr.addstr(0, 0, "              Blissful Nvidia CLI Tool", curses.color_pair(5))
-        stdscr.addstr(1, 0, "-------------------------------------------------------")
+        stdscr.addstr(0, 0, "                Blissful Nvidia CLI Tool", curses.color_pair(5))
+        stdscr.addstr(1, 0, "--------------------------------------------------------")
         stdscr.addstr(3, 18, f"{args.gpu_number} - {gpu_name}", curses.color_pair(1))
         stdscr.addstr(4, 18, f"{core_clock} Mhz core / {mem_clock} Mhz mem", curses.color_pair(clock_color))
         stdscr.addstr(5, 18, f"{current_temperature}°C / Fan: {fan_speed}%", curses.color_pair(temp_color))
